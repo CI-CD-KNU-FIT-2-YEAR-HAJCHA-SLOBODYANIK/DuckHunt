8c3838efe1a58d5c0224032734e52fedb99e9286
Дмитро Хаща <dhasha124d@gmail.com>
2026-02-15 16:59:28 +0200

    implement Score methods for hits, misses and timer

--- code/engine.py
+++ code/engine.py
@@ -7,3 +7,14 @@ class Score:
         self.misses = 0
         self.time_left = 60.0
         self.rounds = 1
+
+    def increment_hit(self):
+        self.hits += 1
+        self.points += 100
+
+    def increment_miss(self):
+        self.misses += 1
+
+    def update_time(self, dt):
+        self.time_left -= dt
+        return self.time_left <= 0
