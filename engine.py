74616aa89719a17f5672a787b4f65a28e66df55f
Nakovalnikov <LetTimMinekraft@gmail.com>
2026-03-04 16:56:53 +0200

    GLOBAL GRAPHICS AND SETTINGS UPDATE
    Неймовірна графіка та якісь три рівня складності

---
+++ engine.py
@@ -0,0 +1,54 @@
+import pygame
+
+# Структура для хранения параметров сложности
+class Difficulty:
+    def __init__(self, name, count, speed, size, color):
+        self.name = name
+        self.count = count  # Целевое колво уток на экране
+        self.speed_mult = speed
+        self.size_mult = size
+        self.color = color  # Цвет для меню настроек
+
+class Score:
+    def __init__(self):
+        self.points = 0
+        self.hits = 0
+        self.misses = 0
+        self.time_left = 60.0
+        self.rounds = 1
+
+    def increment_hit(self):
+        self.hits += 1
+        self.points += 100
+
+    def increment_miss(self):
+        self.misses += 1
+
+    def update_time(self, dt):
+        self.time_left -= dt
+        return self.time_left <= 0
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Verdana", 22, bold=True)
+        self.running = True
+        self.state = "MENU"
+        self.score = Score()
+        
+        self.levels = [
+            Difficulty("ЛЕГКИЙ", 6, 0.75, 1.15, (46, 204, 113)),
+            Difficulty("СРЕДНИЙ", 4, 1.0, 1.0, (241, 196, 15)),
+            Difficulty("СЛОЖНЫЙ", 4, 1.33, 0.85, (231, 76, 60))
+        ]
+        self.current_diff_idx = 1
+
+    def restart(self):
+        self.score = Score()
+        self.state = "PLAYING"
+
+        
+    def get_diff(self):
+        return self.levels[self.current_diff_idx] # Возврат активной сложности
---
+++ entities.py
@@ -0,0 +1,185 @@
+import pygame
+import random
+import math
+
+# Анимация-гиф. Похоже на флипбук
+class Animation:
+    def __init__(self, paths, frame_time, target_size, flip_h=False, rotate_deg=0):
+        self.frames = []
+        for p in paths:
+            img = pygame.image.load(p).convert_alpha()
+            
+            if flip_h: img = pygame.transform.flip(img, True, False)
+            if rotate_deg != 0: img = pygame.transform.rotate(img, rotate_deg)
+
+            img = pygame.transform.scale(img, target_size)
+            
+            self.frames.append(img)
+            
+        self.frame_time = frame_time
+        self.index = 0
+
+    def get_sprite(self, time):
+        if self.frame_time <= 0: 
+            return self.frames[0]
+        
+        index = int(time / self.frame_time) % len(self.frames)
+        return self.frames[index]
+
+# Визуальная декаль. Просто визуальный эффект с некоторой логикой движения. Расчитано на прямолинейное движение или на падение/взлет.
+class VisualDecal:
+    def __init__(self, x, y, impulse_x, impulse_y, weight, resistance, animation):
+        self.pos = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(impulse_x, impulse_y)
+        self.weight = weight  # Положительный вес дает ускорение вниз
+        self.resistance = resistance  # Вычитает (НЕ МНОЖИТ) СКОРОСТЬ не меняя НАПРАВЛЕНИЕ
+        self.animation = animation
+        self.active = True # Статус активности
+        
+        self.time = 0
+
+        fw, fh = self.animation.frames[0].get_size()
+        self.diag = math.sqrt(fw**2 + fh**2)
+
+    def update(self, dt):
+        if not self.active: return
+        
+        self.pos += self.velocity * dt
+        self.time += dt
+
+
+        speed = self.velocity.length()
+        if speed > 0:
+            reduction = self.resistance * dt
+            new_speed = speed - reduction
+            if new_speed > 0:
+                self.velocity.scale_to_length(new_speed)
+            else:
+                self.velocity.update(0, 0)
+
+        self.velocity.y += self.weight * dt
+        
+        if (self.pos.x < -self.diag or self.pos.x > 800 + self.diag or  # Если окно не 800 на 600 то менять тут обязательно!!!
+            self.pos.y < -self.diag or self.pos.y > 600 + self.diag):
+            self.active = False
+
+    def draw(self, screen):
+        sprite = self.animation.get_sprite(self.time)
+        
+        angle = math.degrees(math.atan2(-self.velocity.y, self.velocity.x)) - 90
+        sprite = pygame.transform.rotate(sprite, angle)
+
+        screen.blit(sprite, sprite.get_rect(center=self.pos))
+
+
+class Crosshair:
+    def __init__(self):
+        self.rect = pygame.Rect(0, 0, 30, 30)
+
+    def update_position(self, pos):
+        self.rect.center = pos
+
+
+class Duck:
+    def __init__(self, speed_mult, size_mult, animation):
+        self.time = 0
+        self.speed_mult = speed_mult
+        self.animation = animation # Ссылка на флипбук
+        self.rect = pygame.Rect(0, 0, int(60 * size_mult), int(45 * size_mult))  # По умолчанию хитбокс 60 на 45
+        self.spawn()
+
+    def spawn(self):
+        self.time = 0
+        self.rect.x = random.randint(-150, -60)
+        self.rect.y = random.randint(50, 400)
+        self.velocity = [random.randint(5, 8) * self.speed_mult, random.uniform(-1, 1)]  # Уменьшил на 1 диапазон скорости Х
+    
+    def move(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def update(self, dt):
+        self.time += dt
+
+    def draw(self, screen):
+        sprite = self.animation.get_sprite(self.time)
+        screen.blit(sprite, sprite.get_rect(center=self.rect.center))
+
+
+class Menu:
+    def __init__(self):
+        self.gray = (80, 80, 80)
+        self.gold = (255, 215, 0)
+        self.dark = (30, 30, 40)
+        self.white = (255, 255, 255)
+
+
+        self.btn_start = pygame.Rect(310, 185, 180, 50)
+        self.btn_opts = pygame.Rect(310, 305, 180, 50)
+
+        self.btns_diff = [pygame.Rect(100, 300, 180, 50), pygame.Rect(310, 300, 180, 50), 
+                          pygame.Rect(520, 300, 180, 50)]
+        self.btn_exit = pygame.Rect(310, 450, 180, 50)
+
+    def draw_fancy_ui(self, screen, score_obj, font):
+        # Нижняя панель
+        pygame.draw.rect(screen, self.dark, (0, 530, 800, 70))
+        pygame.draw.line(screen, self.gold, (0, 530), (800, 530), 4)
+        
+        # Полоска времени
+        time_ratio = max(0, score_obj.time_left / 60.0)
+        pygame.draw.rect(screen, (60, 60, 60), (300, 555, 200, 20))
+        pygame.draw.rect(screen, (220, 20, 60), (300, 555, int(200 * time_ratio), 20))
+        
+        # Текст
+        score_txt = font.render(f"SCORE: {score_obj.points:05}", True, self.gold)
+        hits_txt = font.render(f"HITS: {score_obj.hits}", True, self.white)
+        screen.blit(score_txt, (30, 550))
+        screen.blit(hits_txt, (650, 550))
+
+    def draw_overlay(self, screen, state, font, current_idx, levels, score=None):
+        if state == "MENU":
+            self.draw_main(screen, font, current_idx, levels)
+        elif state == "GAMEOVER":
+            self.draw_end(screen, font, score)
+        elif state == "SETTINGS":
+            self.draw_settings(screen, font, current_idx, levels)
+
+
+    def draw_main(self, screen, font, current_idx, levels):
+        screen.fill((20, 20, 30))
+
+        for btn, txt in [(self.btn_start, "START"), (self.btn_opts, "SETTINGS")]:
+            pygame.draw.rect(screen, self.gold, btn, 2)
+            t = font.render(txt, True, self.gold)
+            screen.blit(t, t.get_rect(center=btn.center))
+
+        diff_name = levels[current_idx].name
+        diff_txt = font.render(f"Уровень сложности - {diff_name}", True, self.white)
+        screen.blit(diff_txt, (240, 260))
+
+    def draw_end(self, screen, font, score=None):
+        overlay = pygame.Surface((800, 600), pygame.SRCALPHA)
+        overlay.fill((0, 0, 0, 180))
+        screen.blit(overlay, (0, 0))
+        
+        msg = font.render("HUNT OVER! PRESS 'R' TO RESTART", True, self.gold)
+        res = font.render(f"TOTAL HITS: {score.hits}", True, self.white)
+        screen.blit(msg, (180, 250))
+        screen.blit(res, (310, 310))
+    
+    def draw_settings(self, screen, font, current_idx, levels):
+        screen.fill((20, 20, 30))
+    
+        for i, rect in enumerate(self.btns_diff):
+            level = levels[i]
+            color = level.color if i == current_idx else self.gray
+        
+            pygame.draw.rect(screen, color, rect, 2)
+        
+            txt = font.render(level.name, True, color)
+            screen.blit(txt, txt.get_rect(center=rect.center))
+    
+        pygame.draw.rect(screen, self.gold, self.btn_exit, 1)
+        exit_txt = font.render("ВЫХОД", True, self.gold)
+        screen.blit(exit_txt, exit_txt.get_rect(center=self.btn_exit.center))
