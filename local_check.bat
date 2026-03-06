22d16f6d463e8deb718a8b754ca76764bd587efe
EvheniySlobodyanik <exiletopchik@gmail.com>
2026-03-06 12:12:01 +0200

    setup testing and linting infrastructure

--- local_check.bat
+++ local_check.bat
@@ -2,15 +2,23 @@
 echo ==========================================
 echo [1/2] Perevirka stylu kodu (Flake8)...
 echo ==========================================
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics
+:: Використовуємо python -m, щоб Windows точно знайшла встановлені бібліотеки
+python -m flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+python -m flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics
 
 echo.
 echo ==========================================
 echo [2/2] Zapusk testiv (Pytest)...
 echo ==========================================
-pytest --html=reports/local_report.html --self-contained-html
+:: Створюємо папку для звітів, якщо її немає
+if not exist "reports" mkdir reports
+
+:: Запускаємо тести через python -m
+python -m pytest --html=reports/local_report.html --self-contained-html
 
 echo.
-echo Perevirka zavershena! Zvit zberezheno v papku reports/
+echo ==========================================
+echo Perevirka zavershena!
+echo Zvit testiv: reports/local_report.html
+echo ==========================================
 pause
